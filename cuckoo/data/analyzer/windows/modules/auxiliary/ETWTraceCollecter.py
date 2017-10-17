39bfcad5b6e0b80b5415bb76f9000d0da85f223d
rainkin1993 <rainkin1993@gmail.com>
2017-10-17 14:57:40 +0800

    Add a new guest Auxiliary model ETWTraceCollecter.

---
+++ cuckoo/data/analyzer/windows/modules/auxiliary/ETWTraceCollecter.py
@@ -0,0 +1,110 @@
+# Copyright (C) 2017 rainkin1993
+# This file is part of Cuckoo Sandbox - http://www.cuckoosandbox.org
+# See the file 'docs/LICENSE' for copying permission.
+
+import os
+
+from zipfile import ZipFile
+from subprocess import call
+from lib.common.abstracts import Auxiliary
+from lib.common.exceptions import CuckooPackageError, CuckooDisableModule, CuckooError
+from lib.common.results import upload_to_host
+
+
+class ETWTraceCollecter(Auxiliary):
+    """Allow ETWTraceCollecter to be run on the side."""
+
+    def __init__(self):
+        super(ETWTraceCollecter, self).__init__()
+
+        self.etw_path = self.options.get("etw_path")
+
+        self.output_dir_name = "output"
+        self.corrected_output_dir_name = "corrected_output"
+        self.etw_exe_name = "ETW.exe"
+        self.output_bin_name = "output.bin"
+        self.addressmap_name = "addressmap"
+        self.archive_name = "output.zip"
+        self.preprocess_script_name = "new_preTreat.py"
+        self.parser_name = "parse_cdm_runqing.exe"
+
+    def start(self):
+        """Start the etw collecter"""
+        if not self.options.get("etw"):
+            raise CuckooDisableModule
+        
+        if not self.etw_path:
+            raise CuckooError(
+                "In order to use the ETWTraceCollecter, it is"
+                "required to set the dir path of ETWTraceCollecter"
+                "by the option etw_path"
+            )
+
+        etw_exe_path = os.path.join(self.etw_path, self.etw_exe_name)
+
+        if not os.path.exists(etw_exe_path):
+            raise CuckooPackageError(
+                                     "ETW.exe doesn't exist"
+                                     )
+
+        '''
+        # Register the msdia120.dll         
+        tempdir = os.path.join(bin_path, "res")
+        os.chdir(tempdir)
+        res = os.system("regsvr32 msdia120.dll")
+        res = subprocess.Popen("regsvr32 msdia120.dll", shell = True)
+        time.sleep(0.1)
+        res.kill()
+        os.chdir(curdir)
+        '''
+
+        # Start etw collecter in the background.
+        curdir = os.getcwd()
+        os.chdir(self.etw_path)
+        call([self.etw_exe_name, "-start"])
+        os.chdir(curdir)
+
+    def stop(self):
+        """Stop the etw collecter, preprocess the output,
+        compress the result and upload the archive"""
+
+        # CD into the path of etw collecter
+        curdir = os.getcwd()
+        os.chdir(self.etw_path)
+
+        # Stop etw collecter
+        call([self.etw_exe_name, "-stop"])
+
+        # Parse the output.bin and filter by pid
+        for pid in self.pids:
+            pid_output_dir_path = os.path.join(self.output_dir_name, str(pid))
+            os.mkdir(pid_output_dir_path)
+            call([
+                self.parser_name,
+                str(pid),
+                self.output_bin_name,
+                self.addressmap_name,
+                pid_output_dir_path
+            ])
+
+        # Correct the wrong format data
+        call([
+            "python",
+            self.preprocess_script_name,
+            self.output_dir_name,
+            self.corrected_output_dir_name
+        ])
+
+        # Compress the results as a zip file
+        archive_path = os.path.join(self.etw_path, self.archive_name)
+        archive = ZipFile(archive_path, 'w')
+        for dirpath, dirnames, filenames in os.walk(self.corrected_output_dir_name):
+            for filename in filenames:
+                archive.write(os.path.join(dirpath, filename))
+        archive.close()
+
+        # Upload the results to the host
+        upload_to_host(archive_path, os.path.join("files", self.archive_name))
+
+        # Return back to the original working path
+        os.chdir(curdir)
